fe502edf7ee89dd4af2d4f12aac204bd9d785556
shreyu2403 <shreyas1696@gmail.com>
2018-09-28 09:44:51 +0000

    adding vector program

--- sector_divide.py
+++ sector_divide.py
@@ -3,9 +3,7 @@ import numpy as np
 import scipy
 import sklearn
 import itertools
-import geohash
-import matplotlib
-matplotlib.use('agg')
+
 import matplotlib.pyplot as plt
 #read csv file
 df=pd.read_csv("Raw_data_compactor.csv")
@@ -18,3 +16,35 @@ max_lat=df['fltLatitude'].max()
 min_lon=df['fltLongitude'].min()
 max_lon=df['fltLongitude'].max()
 
+plt.rc('figure', figsize = (14, 7))
+# Font size to 14
+plt.rc('font', size = 14)
+# Do not display top and right frame lines
+plt.rc('axes.spines', top = True, right = True)
+# Remove grid lines
+plt.rc('axes', grid = True)
+# Set backgound color to white
+plt.rc('axes', facecolor = 'white')
+
+def scatterplot(x_data, y_data, x_label, y_label, title):
+
+    # Create the plot object
+    _, ax = plt.subplots()
+
+    # Plot the data, set the size (s), color and transparency (alpha)
+    # of the points
+    ax.scatter(x_data, y_data, s = 30, color = '#539caf', alpha = 0.75)
+
+    # Label the axes and provide a title
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+
+# Call the function to create plot
+scatterplot(x_data = df['fltLatitude']
+            , y_data = df['fltLongitude']
+            , x_label = 'Latitude'
+            , y_label = 'Longitude'
+            , title = 'PLOT')
+
+plt.show()
--- start.py
+++ start.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy
 import sklearn
 import itertools
+import math
 
 import matplotlib
 matplotlib.use('agg')
@@ -49,21 +50,66 @@ df1['distances'] = distance
 #finding diff between each lat and long
 df1['Diff_lat'] = df1['fltLatitude'] - df1['fltLatitude'].shift(1)
 df1['Diff_long'] = df1['fltLongitude'] - df1['fltLongitude'].shift(1)
-#print(df1)
+
 #select only those rows where engine is on
 Igni = df1['IgniStat'] == 1
 An = df1['an1']>3500
 
 df1=df1[Igni & An]
-#want to find out vectors
-#df1['vect']=[df1['fltLatitude']-df1['fltLatitude'].shift(1),df1['fltLongitude']-df1['fltLongitude'].shift(1)]
 #print(df1)
 
-#to plot
-#fig=plt.figure()
-#ax = fig.add_subplot(1,1,1)
-#Variable
-#ax.boxplot(df1['distances'])
-#plt.show()
+#only edited values
+#df1.to_csv('Compactor_modified.csv')
 
-#df1.to_csv('Comapctor_modified.csv')
+#method to find vectors
+def latlong_to_3d(latr, lonr):
+    """Convert a point given latitude and longitude in radians to 3-dimensional space, assuming a sphere radius of one."""
+    return np.array((
+        math.cos(latr) * math.cos(lonr),
+        math.cos(latr) * math.sin(lonr),
+        math.sin(latr)
+    ))
+
+def angle_between_vectors_degrees(u, v):
+    """Return the angle between two vectors in any dimension space,in degrees."""
+    return np.degrees(
+        math.acos(np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))))
+
+# The points in tuple latitude/longitude degrees space
+A = (12.92473, 77.6183)
+B = (12.92512, 77.61923)
+C = (12.92541, 77.61985)
+
+# Convert the points to numpy latitude/longitude radians space
+a = np.radians(np.array(A))
+b = np.radians(np.array(B))
+c = np.radians(np.array(C))
+
+# Vectors in latitude/longitude space
+avec = a - b
+cvec = c - b
+
+# Adjust vectors for changed longitude scale at given latitude into 2D space
+lat = b[0]
+avec[1] *= math.cos(lat)
+cvec[1] *= math.cos(lat)
+
+a=angle_between_vectors_degrees(avec,cvec)
+print(a)
+
+# The points in 3D space
+a3 = latlong_to_3d(*a)
+b3 = latlong_to_3d(*b)
+c3 = latlong_to_3d(*c)
+
+# Vectors in 3D space
+a3vec = a3 - b3
+c3vec = c3 - b3
+
+# Find the angle between the vectors in 2D space
+angle3deg = angle_between_vectors_degrees(a3vec, c3vec)
+
+
+# Print the results
+print('\nThe angle ABC in 2D space in degrees:', angle2deg)
+print('\nThe angle ABC in 3D space in degrees:', angle3deg)
